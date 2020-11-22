28a482212e74eab24df792f7502a950c6dcdb3a4
Eltonleao <3lton.leao@gmail.com>
2020-11-22 01:03:20 -0300

    dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,15 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AlertController } from "@ionic/angular";
+import { LoadingController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.page.html',
-  styleUrls: ['./dashboard.page.scss'],
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.page.html",
+  styleUrls: ["./dashboard.page.scss"],
 })
 export class DashboardPage implements OnInit {
+  constructor(
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async carteira() {
+    const alert = await this.alertCtrl.create({
+      cssClass: "my-custom-class",
+      header: "{valor} de Bradesco",
+      subHeader: "Confirma operação?",
+      message: "após operação: {valor}",
+      buttons: [
+        "cancel",
+        {
+          text: "Okay",
+          handler: async () => {
+            const loading = await this.loadingCtrl.create({
+              cssClass: "my-custom-class",
+              // message: "Please wait...",
+              duration: 2000,
+            });
+            await loading.present();
+          },
+        },
+      ],
+    });
+
+    await alert.present();
   }
-
 }
