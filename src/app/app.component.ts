bbacff611ca43533a5a5dbd47a25017ec99277b8
Eltonleao <3lton.leao@gmail.com>
2020-11-21 23:36:15 -0300

    side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
+// app.component.ts
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, NavController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
 
 @Component({
   selector: 'app-root',
@@ -10,10 +12,33 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+
+  currentPageTitle = 'Dashboard';
+
+  appPages = [
+    {
+      title: 'Dashboard',
+      url: '',
+      icon: 'easel'
+    },
+    {
+      title: 'Timeline',
+      url: '/timeline',
+      icon: 'film'
+    },
+    {
+      title: 'Settings',
+      url: '/settings',
+      icon: 'settings'
+    }
+  ];
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private navCtrl: NavController
   ) {
     this.initializeApp();
   }
@@ -22,6 +47,7 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.navCtrl.navigateForward('tabs/dashboard');
     });
   }
 }
--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -12,8 +12,8 @@ const routes: Routes = [
         loadChildren: () => import('../tab1/tab1.module').then(m => m.Tab1PageModule)
       },
       {
-        path: 'tab2',
-        loadChildren: () => import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+        path: 'dashboard',
+        loadChildren: () => import('../dashboard/dashboard.module').then(m => m.DashboardPageModule)
       },
       {
         path: 'tab3',
