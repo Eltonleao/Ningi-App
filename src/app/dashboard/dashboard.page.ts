1eb2b0374e5386c807f5572567d08fbe3c5be988
Eltonleao <3lton.leao@gmail.com>
2021-01-24 19:33:40 -0300

    gráfico semanal no dashboard

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -86,73 +86,115 @@ export class DashboardPage implements OnInit {
 
     this.ningiService.getNingisSemanais().then(async (prevWeekNingis) => {
       // console.log(prevWeekNingis);
-      var now = new Date();now.setHours(0);now.setMinutes(0);now.setSeconds(0);
-      // now.setDate(now.getDate() - 1);//n dias atras
-
-      var prevWeekDays = [];
-
-      for(var i=0; i < 7; i++){
-        prevWeekDays.push(now.getDay());
-        now.setDate(now.getDate() - 1);
-      }
-      prevWeekDays = prevWeekDays.reverse();
-      console.log(prevWeekDays);
-
-
-      var ningis = {
-        carteira: [{weekday: 0, value: 0}, {weekday: 1, value: 0}, {weekday: 2, value: 0}, {weekday: 3, value: 0}, {weekday: 4, value: 0}, {weekday: 5, value: 0}, {weekday: 6, value: 0}],
-        santander: [{weekday: 0, value: 0}, {weekday: 1, value: 0}, {weekday: 2, value: 0}, {weekday: 3, value: 0}, {weekday: 4, value: 0}, {weekday: 5, value: 0}, {weekday: 6, value: 0}],
-        bradesco: [{weekday: 0, value: 0}, {weekday: 1, value: 0}, {weekday: 2, value: 0}, {weekday: 3, value: 0}, {weekday: 4, value: 0}, {weekday: 5, value: 0}, {weekday: 6, value: 0}],
-        banco_do_brasil: [{weekday: 0, value: 0}, {weekday: 1, value: 0}, {weekday: 2, value: 0}, {weekday: 3, value: 0}, {weekday: 4, value: 0}, {weekday: 5, value: 0}, {weekday: 6, value: 0}],
-      };
-
-      console.log(ningis);
+      var now = new Date(); now.setHours(0); now.setMinutes(0); now.setSeconds(0);
+      // now.setDate(now.getDate() - 2);//n dias atras
+      var diasDaSemana = ['dom', 'seg', 'ter', 'quar', 'quin', 'sex', 'sab'];
+      diasDaSemana[now.getDay()] = 'hoje';
 
       var chartData = [];
       var chartLabels = [];
 
+      var prevWeekDays = [];
+
+      for (var i = 0; i <= 6; i++) {
+        prevWeekDays.push({ weekday: now.getDay(), value: 0 });
+        chartLabels.push(diasDaSemana[now.getDay()]);
+
+        now.setDate(now.getDate() - 1);
+      }
+      // await console.log(prevWeekDays);
+      prevWeekDays = await prevWeekDays.reverse();
+
+      chartLabels = await chartLabels.reverse();
+      // await console.log(prevWeekDays);
+      this.chartLabels = await chartLabels;
+
+      var carteira = JSON.parse(JSON.stringify(prevWeekDays));
+      var bradesco = JSON.parse(JSON.stringify(prevWeekDays));
+      var bancoDoBrasil = JSON.parse(JSON.stringify(prevWeekDays));
+      var santander = JSON.parse(JSON.stringify(prevWeekDays));
+
+      var ningis = await {
+        carteira: carteira,
+        bradesco: bradesco,
+        santander: santander,
+        banco_do_brasil: bancoDoBrasil
+      };
+
+      // await console.log(ningis);
+
+      // return false;
+
+
       await prevWeekNingis.forEach(async ningi => {
         if (ningi.operation == 'spend') {
           var d = new Date(ningi.data_criacao);
-          var date = String(d.getDate());
-          var index = ningis[ningi.source].findIndex(x => x.weekday == d.getDay());
-          // console.log(index, ningis[ningi.source][index]);
-        
-          ningis[ningi.source][index]['value'] = ningis[ningi.source][index]['value']  + parseFloat(ningi.value)
-          
-          // ningis[ningi.source][d.getDay()] = parseFloat(ningis[ningi.source][d.getDay()]) + parseFloat(ningi.value)
-          // chartLabels[d.getDay()] = this.chartLabels[d.getDay()] + "(" + date + ")";
+          // console.log(d, ningis[ningi.source]);
+          for (var item in ningis[ningi.source]) {
+            // var index = ningis[ningi.source].findIndex((x) => x.weekday == d.getDay());
+            if (ningis[ningi.source][item].weekday == d.getDay()) {
+              // ningis[ningi.source][index]['value'] = await parseFloat(ningis[ningi.source][index]['value']) + parseFloat(ningi.value);
+              // console.log(ningis[ningi.source][item]);
+              ningis[ningi.source][item].value = parseFloat(ningis[ningi.source][item].value) + parseFloat(ningi.value);
+            }
+          }
+          // console.log(ningis);
         }
       });
-      // console.log(ningis);
 
-      for (var key in chartLabels) {
-        console.log(key, chartLabels[key]);
-        this.chartLabels[key] = chartLabels[key];
-      };
+      // await console.log(ningis);
+
 
       for (var source in ningis) {
         var obj = { data: [], label: source };
-        // console.log(source, ningis[source]);
-        console.group()
-        console.log(source);
-        for(var index in ningis[source]){
-          console.log(index, ningis[source][index], ningis[source][index].value);
+        for (var index in ningis[source]) {
           obj.data.push(ningis[source][index].value);
         };
-        console.groupEnd()
         chartData.push(obj);
+
+        console.log(source);
+        switch (source) {
+          case 'carteira':
+            obj['backgroundColor'] = '#2dd36f';
+            obj['borderColor'] = '#2dd36f';
+            obj['pointBackgroundColor'] = '#2dd36f';
+            obj['pointBorderColor'] = '#2dd36f';
+            obj['pointBorderColor'] = '#2dd36f';
+            break;
+
+          case 'bradesco':
+            obj['backgroundColor'] = '#eb445a';
+            obj['borderColor'] = '#eb445a';
+            obj['pointBackgroundColor'] = '#eb445a';
+            obj['pointBorderColor'] = '#eb445a';
+            obj['pointBorderColor'] = '#eb445a';
+            break;
+          case 'santander':
+            obj['backgroundColor'] = '#3880ff';
+            obj['borderColor'] = '#3880ff';
+            obj['pointBackgroundColor'] = '#3880ff';
+            obj['pointBorderColor'] = '#3880ff';
+            obj['pointBorderColor'] = '#3880ff';
+            break;
+          case 'banco_do_brasil':
+            obj['backgroundColor'] = '#ffc409';
+            obj['borderColor'] = '#ffc409';
+            obj['pointBackgroundColor'] = '#ffc409';
+            obj['pointBorderColor'] = '#ffc409';
+            obj['pointBorderColor'] = '#ffc409';
+            break;
+        }
       }
-      console.log(chartData);
       env.chartData = chartData;
-    })
+      await console.log(env.chartData);
+    });
+
+    // ###
 
     this.storage.get('user').then(function (user) {
       if (!user) {
         env.navCtrl.navigateForward('/tabs/login');
         env.storage.set('hideTbas', true);
-        window.location.href = '/tabs/login';
-      } else {
       }
     });
   }
@@ -343,43 +385,13 @@ export class DashboardPage implements OnInit {
     this.totalBancoDoBrasil.value = 0;
   }
 
+  copyObj(mainObj) {
+    let objCopy = {}; // objCopy will store a copy of the mainObj
+    let key;
 
-  composeEmail() {
-    // this.emailComposer.getClients().then((apps: []) => {
-    //   // Returns an array of configured email clients for the device
-    // });
-
-    // this.emailComposer.hasClient().then(app, (isValid: boolean) => {
-    //   if (isValid) {
-    //     // Now we know we have a valid email client configured
-    //     // Not specifying an app will return true if at least one email client is configured
-    //   }
-    // });
-
-    // this.emailComposer.hasAccount().then((isValid: boolean) => {
-    //   if (isValid) {
-    //     // Now we know we have a valid email account configured
-    //   }
-    // });
-
-    // this.emailComposer.isAvailable().then(app, (available: boolean) => {
-    //   if (available) {
-    //     // Now we know we can send an email, calls hasClient and hasAccount
-    //     // Not specifying an app will return true if at least one email client is configured
-    //   }
-    // });
-
-    let email = {
-      to: '3lton.leao@gmail.com',
-      bcc: ['3lton.leao@gmail.com'],
-      attachments: [
-      ],
-      subject: 'Cordova Icons',
-      body: 'How are you? Nice greetings from Leipzig',
-      isHtml: true
+    for (key in mainObj) {
+      objCopy[key] = mainObj[key]; // copies each property to the objCopy object
     }
-
-    // Send a text message using default options
-    this.emailComposer.open(email);
+    return objCopy;
   }
 }
