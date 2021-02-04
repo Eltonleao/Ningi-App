8691aa46f99e9b08dec5de13bbe8306b2cba012f
Eltonleao <3lton.leao@gmail.com>
2021-02-03 23:13:02 -0300

    page settings setando hora das notificações

--- src/app/services/notifications.service.ts
+++ src/app/services/notifications.service.ts
@@ -1,29 +1,48 @@
+import { TmplAstRecursiveVisitor } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { Storage } from '@ionic/storage';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class NotificationsService {
 
+  notificationHour: any;
   constructor(
-    public localNotifications: LocalNotifications
+    public localNotifications: LocalNotifications,
+    public storage: Storage
   ) {
+    this.init();
   }
 
 
+  async init(){
+  }
 
-  setNotificationsHour(hour){
-    this.localNotifications.schedule({
+
+  async setNotificationsTime(hour, minute): Promise<any>{
+    await this.localNotifications.schedule({
       title: 'Já registrou seus gastos hoje?',
+      actions: [{ id: 'yes', title: 'Yes' }],
       vibrate: false,
       trigger: {
         count: 1,
         every: {
           hour: hour,
+          minute: minute
         }
       }
     });
 
+    var date = new Date();
+    date.setTime(0);
+    date.setHours(hour);
+    date.setMinutes(minute);
+    this.storage.set('notifications', date).then(()=>{
+      return true;
+    });
   }
+
 }
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { NotificationsService } from "../services/notifications.service";
+import { Storage } from '@ionic/storage';
 
 
 @Component({
@@ -8,12 +9,26 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
-
+  notificationHour: any;
   constructor(
-    public localNotifications: LocalNotifications
-  ) { }
+    public notifications: NotificationsService,
+    public storage: Storage,
+  ) {
+    this.storage.get('notifications').then((time)=>{
+      this.notificationHour = time;
+    })
+  }
 
   ngOnInit() {
+  }
+
+  setNotificationsTime($event){
+
+    var time = new Date($event.detail.value);
+    var hour = time.getHours();
+    var minute = time.getMinutes();
+    
+    this.notifications.setNotificationsTime(hour, minute);
   }
 
 }
