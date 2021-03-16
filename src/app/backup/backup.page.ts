63ea29326d6697139a5b5f0a79e80d4d8d827aa9
Eltonleao <3lton.leao@gmail.com>
2021-03-16 16:04:22 -0300

    backup de ningis em json

--- src/app/backup/backup.page.ts
+++ src/app/backup/backup.page.ts
@@ -26,39 +26,32 @@ export class BackupPage implements OnInit {
   ngOnInit() {
   }
 
-  doBackup() {
-    this.download();
-  }
-
-  download() {
-    this.file.writeFile('/', 'hello.txt', 'hello world').then(function(){
-      alert('ok')
-    }).catch(function(){
-      alert('erro');
+  async doBackup() {
+    const loading = await this.loadingCtrl.create({
+      spinner: "circular"
+    });
+    await loading.present();
+    var env = this;
+    this.ningiService.getNingis(function (ningis: any) {
+      console.log(ningis);
+      env.generateJSON(ningis);
+      loading.dismiss();
     })
   }
 
+  generateJSON(json_data) {
+    var now = new Date();
+    var obj = json_data;
+    var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(obj));
 
+    var a = document.createElement('a');
+    a.href = 'data:' + data;
+    a.download = 'ningis_'+now+'.json';
+    a.innerHTML = 'download JSON';
 
-
-  sendEmail() {
-    let email = {
-      to: 'max@mustermann.de',
-      cc: 'erika@mustermann.de',
-      bcc: ['john@doe.com', 'jane@doe.com'],
-      attachments: [
-        'file://img/logo.png',
-        'res://icon.png',
-        'base64:icon.png//iVBORw0KGgoAAAANSUhEUg...',
-        'file://README.pdf'
-      ],
-      subject: 'Cordova Icons',
-      body: 'How are you? Nice greetings from Leipzig',
-      isHtml: true
-    };
-
-    // Send a text message using default options
-    this.emailComposer.open(email);
+    var container = document.getElementById('download-container');
+    container.appendChild(a);
   }
+
 
 }
